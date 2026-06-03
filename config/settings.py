92bd0d29bc5407c24249c2a48e1adefebae7c2c8
adharshmsdot <adharshms023@gmail.com>
2026-06-04 00:37:43 +0530

    ak

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+     'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
         conn_max_age=600
     )
 }
